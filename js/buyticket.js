7956aad30c8525f0bd474bd1811ffbaa65b7476b
postsovietrepost <71707828+postsovietrepost@users.noreply.github.com>
2021-06-04 22:42:19 +0300

    Create buyticket.js

---
+++ js/buyticket.js
@@ -0,0 +1,36 @@
+let buttonTicket = document.getElementById('ticketqvadrat3')
+let buttonTicketTwo = document.getElementById('ticketqvadrat4')
+
+buttonTicket.addEventListener('click', function() {
+  swal({
+  title: "Купить билет",
+  buttons: {
+    cancel: "Отмена",
+    confirm:"Купить"
+  }
+})
+.then((val) => {
+  if (val) {
+    swal({
+      title: "Спасибо за покупку!"
+    })
+  }
+})
+})
+
+buttonTicketTwo.addEventListener('click', function() {
+  swal({
+  title: "Купить билет",
+  buttons: {
+    cancel: "Отмена",
+    confirm:"Купить"
+  }
+})
+.then((val) => {
+  if (val) {
+    swal({
+      title: "Спасибо за покупку!"
+    })
+  }
+})
+})

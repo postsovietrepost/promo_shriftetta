a2e715536584b8831980c3b4fa5ce89da04312f2
postsovietrepost <71707828+postsovietrepost@users.noreply.github.com>
2021-06-05 19:11:45 +0300

    Update burger.js

--- js/burger.js
+++ js/burger.js
@@ -1,5 +1,16 @@
-document.getElementById("trigger").onclick = function() {open()};
+document.getElementById("burger2_r").onclick = function() {open()};
 
 function open() {
     document.getElementById("menu").classList.toggle("show");
 }
+
+$(document).ready(function(){
+  $('#burger2_r').click(
+      function(){
+          $('#burger2_r').toggleClass('.burgermenu');
+      },
+      function(){
+          $('.burgermenu').css('visibility', 'visible')
+      },
+  );
+  })
